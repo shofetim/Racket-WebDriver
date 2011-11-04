7a395b28a172192f1189528bdc952e1bd0faffe2
shofetim <jordan@noionlabs.com>
2011-11-04 15:52:17 -0600

    WIP yet more functions

--- main.rkt
+++ main.rkt
@@ -102,6 +102,126 @@
         (error "No session"))
       (request "/session" 'get (string-append "/" current-session "/screenshot")))
 
+    (define/public (get-ime-available_engines)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/ime/available_engines/")))
+
+    (define/public (get-ime-active-engine)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/ime/active_engine/")))
+
+    (define/public (get-ime-activated)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/ime/activated/")))
+
+    (define/public (get-cookie)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/cookie/")))
+
+    (define/public (get-source)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/source")))
+
+    (define/public (get-title)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/title")))
+
+    (define/public (get-element arg)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/element/" arg)))
+
+    (define/public (get-element-text arg)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/element/" arg "/text")))
+
+    (define/public (get-element-name arg)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/element/" arg "/name")))
+
+    (define/public (get-element-selected arg)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session 
+                                              "/element/" arg "/selected")))
+
+    (define/public (get-element-enabled)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/element-enabled")))
+
+    (define/public (get-element-attribute arg)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session 
+                                              "/element-attribute/" arg)))
+
+    (define/public (get-element-equals arg1 arg2)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session 
+                                              "/element/" arg1 "/equals/" arg2)))
+
+    (define/public (get-element-displayed arg)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/element/" 
+                                              arg "/displayed")))
+
+    (define/public (get-element-location arg)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/element/"
+                                              arg "/location")))
+
+    (define/public (get-element-location-in-view arg)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/element"
+                                              arg "/location_in_view")))
+
+    (define/public (get-element-size arg)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/element"
+                                              arg "/size")))
+
+    (define/public (get-element-css arg1 arg2)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/element"
+                                              arg1 "/css/" arg2)))
+
+    (define/public (get-orientation)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/orientation")))
+
+    (define/public (get-alert-text)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'get (string-append "/" current-session "/alert_text")))
+
+    (define/public (delete-window)
+      (when (null? current-session)
+        (error "No session"))
+      (request "/session" 'delete (string-append "/" current-session "/window")))
+
+    (define/public (delete-cookie [name null])
+      (when (null? current-session)
+        (error "No session"))
+      (if (null? name)
+          (request "/session" 'delete (string-append "/" current-session "/cookie"))
+          (request "/session" 'delete (string-append "/" current-session "/cookie/" name))))
+
 
 
     ;;Abstractions over protocol
@@ -136,19 +256,5 @@
          delete-pure-port
          (λ (ip)
             (read-json ip)))]
-       [else (error "http method not implemented")]))
-    ))
-
-(define a (new webdriver))
-
-;;(send a get-status)
-;;(send a post-session)
-;;(send a get-session (send a post-session))
-;;(send a delete-session (send a post-session))
-(begin
-  (send a new-session)
-  ;; (send a get-current-session-id)
-  ;; (send a get-window-handle)
-  (send a post-url "http://www.google.com")
-  )
+       [else (error "http method not implemented")]))))
 
